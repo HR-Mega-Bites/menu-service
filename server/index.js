002f52bdfcef6321a21120eb5da33a5fb3a4654a
Carey Lee <careyl96@gmail.com>
2018-04-29 14:50:11 -0700

    added redis

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const parser = require('body-parser');
 const path = require('path');
 const pgClient = require('../database/index.js');
+const redisClient = require('../database/redis.js');
 
 const app = express();
 
@@ -17,24 +18,32 @@ app.get('/recipes/:id/menu', (req, res) => {
   const wineQuery = `SELECT w.* FROM pairings p 
                      INNER JOIN wines w ON w.id = p.wineid 
                      WHERE p.recipeid = ${id}`;
-  pgClient.query(recipeQuery, (err, recipeInfo) => {
-    if (err) {
-      res.status(500);
-      res.send(err);
-      return;
+
+  redisClient.get(id, (err, result) => {
+    if (result) { // if recipe info is cached in redis, send cached result to client
+      res.send(JSON.parse(result));
+    } else { // otherwise query the database and save info to cache
+      pgClient.query(recipeQuery, (err, recipeInfo) => {
+        if (err) {
+          res.status(500);
+          res.send(err);
+          return;
+        }
+        pgClient.query(wineQuery, (err, winePairings) => {
+          if (err) {
+            res.status(500);
+            res.send(err);
+            return;
+          }
+          const recipe = recipeInfo.rows[0];
+          if (recipe) {
+            recipe.wines = winePairings.rows;
+          }
+          redisClient.set(id, JSON.stringify(recipe));
+          res.send(recipe);
+        });
+      });
     }
-    pgClient.query(wineQuery, (err, winePairings) => {
-      if (err) {
-        res.status(500);
-        res.send(err);
-        return;
-      }
-      const recipe = recipeInfo.rows[0];
-      if (recipe) {
-        recipe.wines = winePairings.rows;
-      }
-      res.send(recipe);
-    });
   });
 });
 
